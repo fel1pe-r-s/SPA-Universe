ebeda6af9cd5eb396b3b5e32908e359d8f9680cc
Felipe Silva <1felipersilva1@gmail.com>
2023-07-29 23:46:02 -0300

    configurando estilos

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,12 @@
-import { Router } from "./router.js"
+import { Router } from "./router.js";
 const router = new Router();
 
-router.add('/','/pages/home.html')
-router.add( '/universe', '/pages/universe.html')
-router.add('/exploration', '/pages/exploration.html')
-router.add(404, '/pages/404.html')
+router.add("/", "src/pages/home.html");
+router.add("/universe", "src/pages/universe.html");
+router.add("/exploration", "src/pages/exploration.html");
+router.add(404, "src/pages/404.html");
 
+router.handle();
 
-router.handle()
-
-window.onpopstate = () => router.handle()
-window.route = () => router.route()
+window.onpopstate = () => router.handle();
+window.route = () => router.route();
--- src/js/router.js
+++ src/js/router.js
@@ -1,23 +1,27 @@
+const home = document.querySelector("#home");
+const universe = document.querySelector("#universe");
+const exploration = document.querySelector("#exploration");
+
 export class Router {
-  routes = {}
-  route(event){
-    event = event || window.event 
-    event.preventDefault()
-    window.history.pushState({},"", event.target.href)
-    this.handle()
+  routes = {};
+  route(event) {
+    event = event || window.event;
+    event.preventDefault();
+    window.history.pushState({}, "", event.target.href);
+    this.handle();
   }
 
-  add(routeName, page){
+  add(routeName, page) {
     this.routes[routeName] = page;
   }
-  
-  handle(){
-    const { pathname } = window.location
-    const routePage = this.routes[pathname] || this.routes[404]
-  
-    fetch(routePage)
-      .then(data =>data.text())
-      .then(html => document.getElementById('app').innerHTML = html)   
-  }
 
+  handle() {
+    const { pathname } = window.location;
+    const routePage = this.routes[pathname] || this.routes[404];
+
+    console.log(pathname);
+    fetch(routePage)
+      .then((data) => data.text())
+      .then((html) => (document.getElementById("app").innerHTML = html));
+  }
 }
